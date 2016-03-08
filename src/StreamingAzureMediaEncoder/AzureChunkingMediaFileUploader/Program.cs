3f8743d72dda27d95728f7a6911ebdf886898f70
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2016-03-08 09:42:47 +0100

    first version, which uses the existing uploader by chgeuer to upload a file to a blob

---
+++ src/StreamingAzureMediaEncoder/AzureChunkingMediaFileUploader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using LargeFileUploader;
+
+namespace AzureChunkingMediaFileUploader
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if(args.Length<3)
+            {
+                Help();
+                return -1;
+            }
+
+            var fileToUpload = args[0];
+            var containerName = args[1];
+            var connectionString = args[2];
+
+            LargeFileUploaderUtils.UploadAsync(fileToUpload, connectionString, containerName, (sender, i) =>
+            {
+                Console.WriteLine(i);
+            });
+
+            Console.ReadLine();
+            return 0;
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("Azure Chunking Media File Uploader");
+            Console.WriteLine("USAGE: AzureChunkingMediaFileUploader.exe <UploadFile> <Container> <ConnectionString>");
+        }
+    }
+}
