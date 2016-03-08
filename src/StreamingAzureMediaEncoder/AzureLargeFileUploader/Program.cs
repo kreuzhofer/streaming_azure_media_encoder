e09526dd03481ba2cacbc6fa6ad05a8c9b47348d
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2016-03-08 13:21:24 +0100

    add additional file uploader project

---
+++ src/StreamingAzureMediaEncoder/AzureLargeFileUploader/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LargeFileUploader;
+
+namespace AzureLargeFileUploader
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Help();
+                return -1;
+            }
+
+            var fileToUpload = args[0];
+            var containerName = args[1];
+            var connectionString = args[2];
+
+            LargeFileUploaderUtils.UploadAsync(fileToUpload, connectionString, containerName, (sender, i) =>
+            {
+                Console.WriteLine(i);
+            });
+
+            Console.ReadLine();
+            return 0;
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("Azure Large File Uploader");
+            Console.WriteLine("USAGE: AzureLargeFileUploader.exe <UploadFile> <Container> <ConnectionString>");
+        }
+    }
+}
