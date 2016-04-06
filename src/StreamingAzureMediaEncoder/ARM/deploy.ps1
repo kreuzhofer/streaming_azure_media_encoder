0fa9550f929d72b26ddad3f192bd6e67ead90546
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2016-04-06 11:26:05 +0200

    profiling

--- src/StreamingAzureMediaEncoder/ARM/deploy.ps1
+++ src/StreamingAzureMediaEncoder/ARM/deploy.ps1
@@ -9,14 +9,15 @@ Param(
 	[string]$adminPassword,
 	[string]$subscriptionId,
 	[string]$resourceGroupName,
-	[string]$deploymentName,
+	[string]$deploymentId,
 	[string]$targetStorageAccountResourceGroup,
 	[string]$targetStorageAccountName,
-	[string]$instanceCount,
 	[string]$serviceBits
 )
 
-$location = "northeurope"
+$location = "westeurope"
+$instanceCount = 3;
+$vmSize = "Standard_G5";
 
 # Login to Azure
 Login-AzureRmAccount
@@ -25,11 +26,11 @@ Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionID $subscriptionId
 
 # Create Resource group
-New-AzureRmResourceGroup -Name $deploymentName -Location "$location"
+New-AzureRmResourceGroup -Name $resourceGroupName -Location "$location" -Force
 
-$parameters = @{"adminUsername"="$adminUsername"; "adminPassword" = "$adminPassword"; "dnsNameForPublicIP" = "$deploymentName"; "instanceCount" = "$instanceCount"; "serviceBits" = "$serviceBits"; "targetStorageAccountResourceGroup" = "$targetStorageAccountResourceGroup"; "targetStorageAccountName" = "$targetStorageAccountName" }
+$parameters = @{"adminUsername"="$adminUsername"; "adminPassword" = "$adminPassword"; "deploymentId" = "$deploymentId"; "dnsNameForPublicIP" = "$deploymentId"; "instanceCount" = $instanceCount; "serviceBits" = "$serviceBits"; "targetStorageAccountResourceGroup" = "$targetStorageAccountResourceGroup"; "targetStorageAccountName" = "$targetStorageAccountName"; "vmSize" = "$vmSize" }
 
 # Deploy ARM template
-New-AzureRmResourceGroupDeployment -Name "$deploymentName" -ResourceGroupName "$resourceGroupName" -TemplateFile azuredeploy.json -TemplateParameterObject $parameters
+New-AzureRmResourceGroupDeployment -Name "$deploymentId" -Mode Complete -ResourceGroupName "$resourceGroupName" -TemplateFile azuredeploy.json -TemplateParameterObject $parameters -Force
 
 
